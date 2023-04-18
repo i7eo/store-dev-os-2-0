0918d5e75bff1f0a14cf0d2129e2db42175d8582
i7eo <admin@i7eo.com>
2023-04-18 10:42:18 +0800

    feat: tailwind scope

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.{liquid}'],
+  content: [
+    './layout/**/*.{liquid}',
+    './sections/**/*.{liquid}',
+    './snippets/**/*.{liquid}',
+    './templates/**/*.{liquid}',
+  ],
   theme: {
     extend: {},
   },
